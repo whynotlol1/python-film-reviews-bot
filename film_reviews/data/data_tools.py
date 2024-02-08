615d73d6d9863c2b26858e2f6745f5c7aa8379a2
cat dev <117660464+whynotlol1@users.noreply.github.com>
2024-02-08 14:55:53 +0300

    Create data_tools.py

---
+++ film_reviews/data/data_tools.py
@@ -0,0 +1,78 @@
+# (c) cat dev 2024
+
+import requests
+import sqlite3
+import random
+import json
+import os
+
+conn = sqlite3.connect("../data/data.db", check_same_thread=False)
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS films (
+    id INTEGER,
+    name TEXT,
+    review_count INTEGER
+)
+""")
+conn.commit()
+cur.execute("""
+CREATE TABLE IF NOT EXISTS bugs (
+    description TEXT
+)
+""")
+conn.commit()
+
+last_id_check = cur.execute("SELECT id FROM films WHERE 1=1").fetchone()
+if last_id_check is not None:
+    last_id_obj = cur.execute("SELECT id FROM films WHERE 1=1").fetchall()
+    last_id = last_id_obj[len(last_id_obj) - 1][0]
+else:
+    last_id = 0
+
+
+def check_film(film_name):
+    req_txt = ""
+    for word in film_name.split(" "):
+        req_txt += f"{word}_"
+    req_txt += "(film)"
+    req = requests.get(f"https://en.wikipedia.org/wiki/{req_txt}")
+    if req.status_code == 200:
+        return "Found"
+    else:
+        return "Not Found"
+
+
+def leave_review(review_form):
+    global last_id
+    name = review_form["film_name"]
+    check = cur.execute("SELECT * FROM films WHERE name=?", (name,)).fetchone()
+    if check is None:
+        cur.execute("INSERT INTO films VALUES (?,?,?)", ((last_id := last_id + 1), name, 1))
+        conn.commit()
+        film_id = last_id
+    else:
+        obj = cur.execute("SELECT review_count, id FROM films WHERE name=?", (name,)).fetchone()
+        review_count, film_id = obj[0], obj[1]
+        cur.execute("UPDATE films SET review_count=? WHERE name=?", (review_count + 1, name))
+        conn.commit()
+    with open(f"../data/reviews/{film_id}_{random.randint(1000, 1000001)}.json", "w") as f:
+        f.write(json.dumps(review_form))
+
+
+def get_reviews(film_name):
+    check = cur.execute("SELECT * FROM films WHERE name=?", (film_name,)).fetchone()
+    if check is not None:
+        obj = cur.execute("SELECT id FROM films WHERE name=?", (film_name,)).fetchone()
+        film_id = obj[0]
+        reviews = {}
+        for num in range(1000, 1000001):
+            path = f"../data/reviews/{film_id}_{num}.json"
+            if os.path.isfile(path):
+                with open(path, "r") as f:
+                    json_obj = json.loads(f.read())
+                    reviews[json_obj["rating"]] = json_obj["review_text"]
+        return reviews
+    else:
+        return None
