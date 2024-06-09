82f1e2ed66147dc1b47fa52a7f768336169047e7
cat dev <bigcatchonker@mail.ru>
2024-06-09 05:49:01 +0300

    The bot SHOULD now be infinitely polling

--- film_reviews/run.py
+++ film_reviews/run.py
@@ -1,9 +1,18 @@
 # (c) cat dev 2024
 
+from requests import exceptions
 from film_reviews.data import data_api
 from film_reviews.bot.bot import bot
+import sys
+import os
 
 if __name__ == "__main__":
     print("Bot is running...")
     data_api.start()
-    bot.polling(none_stop=True)
+    try:
+        bot.infinity_polling(timeout=10, long_polling_timeout=5)
+    except (exceptions.ConnectionError, exceptions.ReadTimeout) as e:
+        sys.stdout.flush()
+        os.execv(sys.argv[0], sys.argv)
+    else:
+        bot.infinity_polling(timeout=10, long_polling_timeout=5)
